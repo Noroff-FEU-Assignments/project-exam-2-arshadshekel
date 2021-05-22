314dacd899f27766ef4d0253329ccfd28202e0bf
Arshad Shakil <arshad@shakil.no>
2021-05-22 21:40:20 +0200

    HALLELUJAH

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -4,10 +4,4 @@ export const CONTACTURL = "https://vast-brook-39076.herokuapp.com/contact-forms/
 export const ENQUIRYURL = "https://vast-brook-39076.herokuapp.com/Enquiries/";
 export const ADDHOTELS = "https://vast-brook-39076.herokuapp.com/Hotels/";
 
-/* export const API = "http://localhost:1337/hotels/";
-export const LOGINURL = "http://localhost:1337/auth/local";
-export const CONTACTURL = "http://localhost:1337/contact-forms";
-export const ENQUIRYURL = "http://localhost:1337/Enquiries";
-export const ADDHOTELS = "http://localhost:1337/Hotels/"; */
- 
 export const GMAPSAPI = "AIzaSyAno-J9N2_6rNDtoA0VmkJU5IqVXQq7DEk";
